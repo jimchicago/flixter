4d47388565e237d7c6459d7f3b55ddb62c3f5beb
Jim Hinckley <jim@jhwm.net>
2017-02-07 19:13:23 +0000

    Added 'Next Lesson' buttons, finished footer, added static pages (linked in footer)

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   resource :dashboard, only: [:show]
   root 'static_pages#index'
+  get 'team', to: 'static_pages#team'
+  get 'careers', to: 'static_pages#careers'
+  get 'privacy', to: 'static_pages#privacy'
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
@@ -11,7 +14,7 @@ Rails.application.routes.draw do
     resources :sections, only: [:update]
     resources :lessons, only: [:update]
     resources :sections, only: [] do
-      resources :lessons, only: [:new, :create]
+      resources :lessons, only: [:create]
     end
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:create] do
